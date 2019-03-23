e7cc72189ffbd9194c6b5f0aa9d317f6c4c187c6
Amin Abromand <amin.abromand@metaproc.com>
2019-03-23 12:05:13 +0100

    Initial Commit

---
+++ SimpleRestaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleRestaurant
+//
+//  Created by Amin Abromand on 23.03.19.
+//  Copyright © 2019 Amin Abromand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SimpleRestaurantTests/SimpleRestaurantTests.swift
@@ -0,0 +1,34 @@
+//
+//  SimpleRestaurantTests.swift
+//  SimpleRestaurantTests
+//
+//  Created by Amin Abromand on 23.03.19.
+//  Copyright © 2019 Amin Abromand. All rights reserved.
+//
+
+import XCTest
+@testable import SimpleRestaurant
+
+class SimpleRestaurantTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
